ca125eba510790d9f355e75b15e39b4743da456e
pyama <pyama@pepabo.com>
2024-08-25 10:22:25 +0900

    bump

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import find_packages, setup
 
 setup(
     name="mimizuku",
-    version="0.2.16",
+    version="0.2.17",
     packages=find_packages(),
     install_requires=[
         "numpy",
