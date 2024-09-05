32c9d823d03ca4f59bf9d0c9584b970f46d010df
pyama <pyama@pepabo.com>
2024-09-06 02:04:46 +0900

    📦 新機能(setup.py): バージョンを0.2.30から0.2.31に更新 バージョン0.2.30から0.2.31にバージョンを更新しました。この更新には新しい機能や修正が含まれている可能性があります。

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import find_packages, setup
 
 setup(
     name="mimizuku",
-    version="0.2.30",
+    version="0.2.31",
     packages=find_packages(),
     install_requires=[
         "numpy",
